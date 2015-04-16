712ed6c16071ce4fef4c7a3036f2af0bb9ab2c57
Ryan Zolper <zolper11@gmail.com>
2015-04-16 18:16:34 -0400

    aosdjfal

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def free(user)
     @user = user
-    mail(to: user.email, subject: 'Welcome to Arrange an Exchange!', from: 'signup@arrangeanexchange.com')
+    mail(to: user.email, subject: 'Welcome to Arrange an Exchange!', from: 'Arrange An Exchange <signup@arrangeanexchange.com>')
   end
 end
