db566c0d3c95074dbe260eae24f5406436afb88e
Ryan Zolper <zolper11@gmail.com>
2015-04-25 16:45:42 -0400

    val

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
+  validates :email, presence: true
 end
